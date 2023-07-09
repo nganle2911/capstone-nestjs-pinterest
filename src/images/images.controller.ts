21a0d1f4d7b6274189378d25729aa26f9efd4a7e
Ngan LE <ngan.ltkn2911@gmail.com>
2023-07-09 17:04:49 +0200

    Get image by name

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -7,9 +7,16 @@ import { UpdateImageDto } from './dto/update-image.dto';
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
+  // Get image list 
   @Get('get-image-list')
   getImageList(){
     return this.imagesService.getImageList();
   }
   
+  // Get images by image_name 
+  @Get("get-image-by-name/:image_name")
+  getImageByName(@Param("image_name") image_name: string) {
+    return this.imagesService.getImageByName(image_name);
+  }
+
 }
--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, Param } from '@nestjs/common';
 import { CreateImageDto } from './dto/create-image.dto';
 import { UpdateImageDto } from './dto/update-image.dto';
 import { PrismaClient } from '@prisma/client';
@@ -8,6 +8,27 @@ export class ImagesService {
 
   async getImageList(){
     return await this.prisma.images.findMany()
+  }
+
+  async getImageByName(imgName: string) {
+    try {
+      // Find image by name 
+    let imageByName = await this.prisma.images.findFirst({
+      where: {
+        image_name: imgName
+      }
+    }); 
+
+    if (imageByName) {
+      return imageByName; 
+    } else {
+      throw new HttpException("image_name not found!", 404); 
+    }
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(error.response, error.status); 
+    }
+    
   }
   
 }
