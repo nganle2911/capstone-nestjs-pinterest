d312f966a2a2a616922777fb1b24b8b524f09e47
Trinh Hoai Thanh <91533983+trinhhoaithanh@users.noreply.github.com>
2023-07-17 21:23:36 +0700

    modify database

--- db_pinterest.sql
+++ db_pinterest.sql
@@ -11,17 +11,7 @@ CREATE TABLE users(
 	avatar VARCHAR(250)
 );
 
-INSERT INTO users (email, pass_word, full_name, age, avatar) VALUES
-('sophia@example.com', '1234', 'Sophia', 25, 'sophia.jpg'),
-('william@example.com', '6789', 'William', 30, 'william.jpg'),
-('james@example.com', '141002', 'James', 28, 'james.jpg'),
-('emily@example.com', '291193', 'Emily', 32, 'emily.jpg'),
-('mia@example.com', '140296', 'Mia', 27, 'mia.jpg'),
-('isabelle@example.com', '230690', 'Isabelle', 31, 'isabelle.jpg'),
-('liam@example.com', '100495', 'Liam', 29, 'liam.jpg'),
-('ethan@example.com', '190999', 'Ethan', 26, 'ethan.jpg'),
-('john@example.com', '290402', 'John', 33, 'john.jpg'),
-('noah@example.com', '230502', 'Noah', 24, 'noah.jpg');
+
 
 CREATE TABLE images(
 	image_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -32,17 +22,6 @@ CREATE TABLE images(
 	FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-INSERT INTO images (image_name, image_path, description, user_id) VALUES
-('sky', 'public/img/image1.jpg', 'Blue sky', 1),
-('dog', 'public/img/image2.jpg', 'Cute dog', 2),
-('villa', 'public/img/image3.jpg', 'Beautiful villa', 5),
-('natural-landscape', 'public/img/image4.jpg', 'A beaufitul landscape with nature', 4),
-('milk-tea', 'public/img/image5.jpg', 'Bubble tea of matcha', 5),
-('web-dev-skills', 'public/img/image6.jpg', 'necessary web developer skills', 7),
-('full-stack', 'public/img/image7.jpg', 'How to become a full stack developer in 2023', 7),
-('souris-de-paris', 'public/img/image8.jpg', 'Learning French', 2),
-('strawberry-cake', 'public/img/image9.jpg', 'Vegan strawberry cake recipe', 1),
-('salad', 'public/img/image10.jpg', 'How to make a healthy salad', 1);
 
 CREATE TABLE comments(
 	comment_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -54,17 +33,7 @@ CREATE TABLE comments(
 	content VARCHAR(250)
 );
 
-INSERT INTO comments (user_id, image_id, comment_date, content) VALUES
-(1, 1, '2023-07-02', 'Comment 1 for image 1'),
-(2, 1, '2023-07-02', 'Comment 2 for image 1'),
-(3, 2, '2023-07-02', 'Comment 1 for image 2'),
-(4, 3, '2023-07-02', 'Comment 1 for image 3'),
-(5, 4, '2023-07-02', 'Comment 1 for image 4'),
-(6, 5, '2023-07-02', 'Comment 1 for image 5'),
-(7, 6, '2023-07-02', 'Comment 1 for image 6'),
-(8, 7, '2023-07-02', 'Comment 1 for image 7'),
-(9, 8, '2023-07-02', 'Comment 1 for image 8'),
-(10, 9, '2023-07-02', 'Comment 1 for image 9');
+
 
 CREATE TABLE save_img(
 	save_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -75,15 +44,4 @@ CREATE TABLE save_img(
 	save_date DATE
 );
 
-INSERT INTO save_img (user_id, image_id, save_date) VALUES
-(1, 1, '2023-07-02'),
-(2, 1, '2023-07-02'),
-(3, 2, '2023-07-02'),
-(4, 3, '2023-07-02'),
-(5, 4, '2023-07-02'),
-(6, 5, '2023-07-02'),
-(7, 6, '2023-07-02'),
-(8, 7, '2023-07-02'),
-(9, 8, '2023-07-02'),
-(10, 9, '2023-07-02');
 
